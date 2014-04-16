616c19819b2cb733ed379a5b14b1a1a888f47f3a
Tara Nelson & Alex Wood <student@epicodus.com>
2014-04-16 11:52:31 -0700

    prevent a non-logged-in user from adding a property and will prevent a logged-in user from adding a property on someone else's profile

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,15 @@
 class PropertiesController < ApplicationController
+
+  before_action :authenticate_user!, :only => [:new, :create]
+
   def new
     @property = Property.new
     @user = current_user
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @property = Property.find(params[:id])
   end
 
   def create
